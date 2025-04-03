42202c6c70be39db1501cfa842e2b5a2fb5e5e5b
Brayden Rayner <braydenrayner@outlook.com>
2025-04-03 17:46:47 -0300

    Lexer is now working

---
+++ src/lexer.rs
@@ -0,0 +1,205 @@
+use core::fmt;
+use std::fmt::write;
+
+#[derive(Debug)]
+pub enum Token {
+    // Miscellaneaous
+    EOF,
+
+    // Operations
+    Plus, Minus, Asterisk, Slash, Caret,
+    Modulus,
+
+    // Parentheses
+    LeftParen, RightParen,
+
+    // Literals
+    Number(f64),
+}
+
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            Token::EOF => write!(f, "EOF"),
+            Token::Plus => write!(f, "Plus"),
+            Token::Minus => write!(f, "Minus"),
+            Token::Asterisk => write!(f, "Asterisk"),
+            Token::Slash => write!(f, "Slash"),
+            Token::Caret => write!(f, "Caret"),
+            Token::Modulus => write!(f, "Modulus"),
+            Token::LeftParen => write!(f, "LeftParen"),
+            Token::RightParen => write!(f, "RightParen"),
+            Token::Number(n) => write!(f, "Number({})", n),
+        }
+    }
+}
+
+
+#[derive(Debug)]
+pub struct LexerError {
+    pub message: String
+}
+
+impl LexerError {
+    fn new(message: &str) -> Self {
+        LexerError { message: message.to_string() }
+    }
+}
+
+
+pub struct Lexer<'a> {
+    source: &'a str,
+    current_position: usize,
+    token_start: usize,
+    token_list: Vec<Box<Token>>
+}
+
+
+impl<'a> Lexer<'a> {
+    pub fn new(source: &'a str) -> Self {
+        Self { 
+            source,
+            current_position: 0,
+            token_start: 0,
+            token_list: vec![]
+        }
+    }
+
+    fn has_next(&self) -> bool {
+        (self.current_position as usize) < self.source.len()
+    }
+
+    fn advance(&mut self) -> Result<char, LexerError> {
+        let next_char = self.source
+            .chars()
+            .nth(self.current_position);
+
+        match next_char {
+            Some(n) => {
+                self.current_position += 1;
+                Ok(n)
+            },
+            None => Err(LexerError::new("Cannot read past the end of the source."))
+        }
+    }
+
+    fn peek(&self) -> char {
+        if !self.has_next() {
+            return '\0'
+        }
+
+        self.source
+                .chars()
+                .nth(self.current_position)
+                .unwrap()
+    }
+
+    fn add_token(&mut self, token: Box<Token>) {
+        self.token_list.push(token);
+    }
+
+    fn scan_number(&mut self) -> Result<(), LexerError> {
+        while self.has_next() && self.peek().is_digit(10) {
+            if let Err(e) = self.advance() {
+                return Err(e)
+            }
+        }
+
+        if self.peek() == '.' {
+            if let Err(e) = self.advance() {
+                return Err(e)
+            }
+            while self.has_next() && self.peek().is_digit(10) {
+                if let Err(e) = self.advance() {
+                    return Err(e)
+                }
+            }
+        }
+
+        let lexeme = &self.source[self.token_start..self.current_position];
+        let value: Result<f64, std::num::ParseFloatError> = lexeme.parse();
+
+        match value {
+            Ok(n) => {
+                self.add_token(Box::new(Token::Number(n)));
+                Ok(())
+            },
+            Err(_) => Err(LexerError::new("Failed to parse float."))
+        }
+
+    }
+
+    fn scan_next(&mut self) -> Result<(), LexerError> {
+        let next = self.advance();
+        if let Err(e) = next {
+            return Err(e);
+        }
+
+        let ch = next.unwrap();
+        match ch {
+            ' ' => {}
+            '+' => {
+                self.add_token(Box::new(Token::Plus));
+            }
+            '-' => {
+                self.add_token(Box::new(Token::Minus));
+            }
+            '/' => {
+                self.add_token(Box::new(Token::Slash));
+            }
+            '*' => {
+                self.add_token(Box::new(Token::Asterisk));
+            }
+            '^' => {
+                self.add_token(Box::new(Token::Caret));
+            }
+            '%' => {
+                self.add_token(Box::new(Token::Modulus));
+            }
+            '(' => {
+                self.add_token(Box::new(Token::LeftParen));
+            }
+            ')' => {
+                self.add_token(Box::new(Token::RightParen));
+            }
+            c if c.is_digit(10) => {
+                if let Err(e) = self.scan_number() {
+                    return Err(e);
+                }
+            }
+            _ => {
+                return Err(LexerError::new("Unrecognized token."))
+            }
+        };
+
+        Ok(())
+    }
+
+    ///
+    /// Convert an input string to a list of tokens.
+    /// 
+    /// # Returns
+    /// A `&Vec<Box<Token>>` or rather a reference to a vector of heap-allocated
+    /// tokens constructed from the input string.
+    /// 
+    pub fn tokenize(&mut self) -> Result<&Vec<Box<Token>>, LexerError> {
+        while self.has_next() {
+            // If scanning the next token produces an error,
+            // return that error.
+            if let Err(e) = self.scan_next() {
+                return Err(e);
+            }
+
+            // Set the start of the current token to the current position.
+            self.token_start = self.current_position;
+        }
+
+        // Add the EOF token.
+        self.add_token(Box::new(Token::EOF));
+
+        // No errors occurred so return a success result and
+        // the list of tokens.
+        Ok(&self.token_list)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::io::{self, Write};
 
+use lexer::Lexer;
+mod lexer;
+
 
 fn run_repl() {
     let mut line = String::new();
@@ -7,13 +10,33 @@ fn run_repl() {
         print!("expr > ");
         io::stdout().flush().unwrap();
 
+        // Read an expression from the user
         io::stdin()
             .read_line(&mut line)
             .unwrap();
 
+        // If the user entered the quit command, break out of the REPL.
         if line.trim() == String::from(".quit") {
             break 'repl;
         }
+
+        // Tokenize the input string.
+        let mut tokenizer = Lexer::new(line.trim());
+        let tokens = tokenizer.tokenize();
+
+        match tokens {
+            Ok(token_list) => {
+                for token in token_list {
+                    println!("{}", *token);
+                }
+            }
+            Err(e) => {
+                eprintln!("{}", e.message);
+            }
+        }
+
+        // Clear the line buffer.
+        line.clear();
     }
 }
 
